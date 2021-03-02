82071e9e05da700d036f905d7c0185bb0682eace
Katerina Andreeva <andikatya@gmail.com>
2021-03-02 20:41:28 +0300

    update

--- src/pages/Page_CV.js
+++ src/pages/Page_CV.js
@@ -4,6 +4,10 @@ import {Transition} from "react-transition-group";
 import CV from './../components/CV';
 
 class Page_CV extends React.PureComponent {
+
+    // HOC возвращает каждый раз НОВЫЙ, обёрнутый компонент
+    // поэтому оборачивать в HOC лучше не внутри render, чтобы не рендерить каждый раз НОВЫЙ компонент
+    CVWithData=withDataLoad()(CV);
           
     render() {
         console.log(this.props.history);
--- src/pages/PagesRouter.js
+++ src/pages/PagesRouter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 
-import Page_Load from './Page_Load';
+//import Page_Load from './Page_Load';
 import Page_CV from './Page_CV';
 import Page_View from './Page_View';
 
@@ -11,8 +11,7 @@ class PagesRouter extends React.Component {
 
         return (
             <React.Fragment>
-            <Route path='/' exact component={Page_Load} />
-            <Route path='/cv' component={Page_CV} />
+            <Route path='/' exact component={Page_CV}/>
             <Route path='/view' component={Page_View} />
             </React.Fragment>
         );
